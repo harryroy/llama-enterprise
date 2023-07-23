fe3c43320fec56d37a17a50481864b660922916c
harryroy <harry.roy@gmail.com>
2023-07-23 13:00:25 -0500

    Update __init__.py
    added comments

--- llama/__init__.py
+++ llama/__init__.py
@@ -1,6 +1,11 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
+# Import the Llama class from the "generation" module within the "llama" package.
+# The Llama class is used for text generation tasks.
 from .generation import Llama
+
+# Import the ModelArgs and Transformer classes from the "model" module within the "llama" package.
+# ModelArgs is used for defining model configuration parameters, and Transformer is the main Transformer model class.
 from .model import ModelArgs, Transformer
+
+# Import the Tokenizer class from the "tokenizer" module within the "llama" package.
+# The Tokenizer class is responsible for tokenizing and encoding text data for use with the Transformer model.
 from .tokenizer import Tokenizer
