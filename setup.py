80d7cd7e678860d931ce78c43c25a913d3b175df
harryroy <harry.roy@gmail.com>
2023-07-23 08:56:15 -0500

    Update setup.py
    added comments to code

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
+# Import required function from setuptools
 from setuptools import find_packages, setup
 
-
+# Helper function to get requirements from a requirements file
 def get_requirements(path: str):
     return [l.strip() for l in open(path)]
 
-
+# Package setup configuration
 setup(
-    name="llama",
-    version="0.0.1",
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt"),
+    name="llama",  # Name of the package
+    version="0.0.1",  # Version of the package
+    packages=find_packages(),  # Find all packages in the current directory and its subdirectories
+    install_requires=get_requirements("requirements.txt"),  # Get required packages from the "requirements.txt" file
 )
